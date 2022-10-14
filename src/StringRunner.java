bd3b98b236140493a4b2a139c87d3ec806fde477
Kelly Xia <Kellyx8@nycstudents.net>
2022-10-14 09:28:31 -0400

    new additions

--- src/StringRunner.java
+++ src/StringRunner.java
@@ -3,13 +3,14 @@ public class StringRunner {
     public static void main (String[] args) {
         Scanner input = new Scanner (System.in);
 
+
         System.out.print("Enter first string: ");
-        String str1 = input.next();
+        String str1 = input.nextLine();
         System.out.println("String length: " + str1.length());
         System.out.println("First Half: " + str1.substring(0,str1.length()/2));
         System.out.println("Second Half: " + str1.substring(str1.length()/2));
         System.out.print("Enter second string: ");
-        String str2 = input.next();
+        String str2 = input.nextLine();
         if (str1.length()>str2.length()) {
             System.out.println(str1 + " is longer");
         } else if (str2.length()>str1.length()){
